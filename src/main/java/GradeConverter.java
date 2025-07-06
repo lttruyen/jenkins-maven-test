3f9d7b15ef27e5716590ecd4504c1bf2cfa7dd78
lttruyen <lttruyen2000@gmail.com>
2025-07-06 09:33:29 +0700

    refactor code for test 4

--- src/main/java/GradeConverter.java
+++ src/main/java/GradeConverter.java
@@ -5,7 +5,7 @@ public class GradeConverter {
             return "A";
         } else if(score <= 89 && score >= 80) {
             return "B";
-        } else if(score <= 79 && score > 70) {
+        } else if(score <= 79 && score >= 70) {
             return "C";
         } else if(score <= 69 && score > 60) {
             return "D";
